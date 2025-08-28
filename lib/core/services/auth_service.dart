70234f171ad820095410f05ea4f4a87b38b54d3b
Isadora Gomes Pinheiro <gomespinheiroisadora@gmail.com>
2025-08-27 21:18:14 -0300

    android

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,1 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  static final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  /// Cadastra usuário com email e senha
+  static Future<String> cadastrarUsuario(String email, String senha) async {
+    try {
+      UserCredential cred = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: senha,
+      );
+      return cred.user!.uid; // retorna o UID do usuário criado
+    } on FirebaseAuthException catch (e) {
+      throw e.message ?? "Erro ao cadastrar usuário";
+    }
+  }
+
+  /// Faz login do usuário com email e senha
+  static Future<String> loginUsuario(String email, String senha) async {
+    try {
+      UserCredential cred = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: senha,
+      );
+      return cred.user!.uid; // retorna o UID do usuário logado
+    } on FirebaseAuthException catch (e) {
+      throw e.message ?? "Erro ao fazer login";
+    }
+  }
+
+  /// Faz logout do usuário
+  static Future<void> logout() async {
+    await _auth.signOut();
+  }
+
+  /// Retorna o usuário atualmente logado (ou null)
+  static User? get usuarioAtual => _auth.currentUser;
+}
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // gerado pelo FlutterFire CLI
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 
 void main() {
   runApp(const MyApp());
